5f9f06e18b2f5946430a3ce7cb5963ae77687a0b
zack6yuan <zack6yuan@gmail.com>
2024-01-20 09:40:47 +0000

    size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,21 +9,21 @@
 int main(void)
 {
 #ifdef __LP64__
-    /* 64-bit architecture */
-    printf("Size of a char: %zu byte(s)\n", sizeof(char));
-    printf("Size of an int: %zu byte(s)\n", sizeof(int));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	/* 64-bit architecture */
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 #else
-    /* 32-bit architecture */
-    printf("Size of a char: %u byte(s)\n", sizeof(char));
-    printf("Size of an int: %u byte(s)\n", sizeof(int));
-    printf("Size of a long int: %u byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %u byte(s)\n", sizeof(float));
+	/* 32-bit architecture */
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 #endif
 
-    return (0);
+	return (0);
 }
 
