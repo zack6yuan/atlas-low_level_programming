1922377b3bf12617a96fa605b5277ce447f9bc63
zack6yuan <zack6yuan@gmail.com>
2024-01-20 09:34:46 +0000

    size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+
+
 /**
- * main - A program that prints the size of various computer types
- * Return: 0 (Success)
+ * main - Prints the various types based on
+ * the computer it is compiled and run on..
+ * Return: Always 0.
  */
+
 int main(void)
 
 {
 	printf("Size of a char: %zu byte(s)\n",sizeof(char));
+
 	printf("Size of a int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
