4eba255891d87f8920bdaed2da841a121b7e4c8a
zack6yuan <zack6yuan@gmail.com>
2024-01-20 09:46:03 +0000

    puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints a line with the puts function
+ * main - A program that prints a line with puts function
  * Return: 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
