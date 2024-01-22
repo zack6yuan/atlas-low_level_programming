d217bfead21ae994dd40d664bcdb8ae1098daa18
zack6yuan <zack6yuan@gmail.com>
2024-01-22 00:57:36 +0000

    sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -6,7 +6,7 @@
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 
-int print_sign(int a)
+int print_sign(int n)
 
 {
 
@@ -18,7 +18,7 @@ int print_sign(int a)
 	else if (n < 0)
 	{
 	_putchar(45);
-	return (-1)
+	return (-1);
 	}
 	else
 	{
