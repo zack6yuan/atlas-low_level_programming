0004a4f31189a345c14486527f8c8807f06c3c59
zack6yuan <zack6yuan@gmail.com>
2024-01-22 23:48:40 +0000

    last digit

--- functions_nested_loops/7-main.c
+++ functions_nested_loops/7-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
     	    int r;
@@ -13,6 +13,6 @@ int main(void)
 	    print_last_digit(0);
 	    r = print_last_digit(-1024);
 	    _putchar('0' + r);
-	    _putchar('\n');
+    	    _putchar('\n');
 	    return (0);
 }
