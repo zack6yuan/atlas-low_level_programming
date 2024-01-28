878797b216dc93a784b49cf7c0fcaafe8c8b05e1
zack6yuan <zack6yuan@gmail.com>
2024-01-28 21:14:30 +0000

    I feel like I am disgoanlly parked in a parallel universe

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,15 +1,45 @@
 #include "main.h"
 
 /**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     */
+ * print_diagonal - Print a diagonal line using backslashes
+ * @n: Number of times the backslash should be printed
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
+
+/**
+ * main - Check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	    print_diagonal(0);
-	        print_diagonal(2);
-		    print_diagonal(10);
-		        print_diagonal(-4);
-			    return (0);
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+
+	return (0);
 }
+
