3df103c67d382441a4d3b9dfbe01b55dce84ae1b
zack6yuan <zack6yuan@gmail.com>
2024-01-28 20:17:44 +0000

    the numbers speak for themselves

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - print the numbers from 0 up to 9
- * Return: the numbers from 0 up to 9
+ * print_numbers - Print the numbers from 0 to 9
+ *
+ * Return: void
  */
-
 void print_numbers(void)
-
 {
-
-	int x; 
+	int x;
 
 	for (x = 0; x <= 9; x++)
 	{
-	_putchar(    x + '0');
+		_putchar(x + '0');
 	}
 	_putchar('\n');
 }
