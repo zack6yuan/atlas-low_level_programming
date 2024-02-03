9fba6c05d333b6fd17b6c39917e211bbb89532cf
zack6yuan <zack6yuan@gmail.com>
2024-02-03 15:26:17 +0000

    string toupper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,15 +8,15 @@
  */
 
 char *string_toupper(char *n)
+{
+        int i;
 
-	int i;
-
-	i = 0;
-	while (n[i] != = '\0')
-	{
-		if (n[i] >= 'a' && n[i] <= 'z')
-			n[i] = n[i] - 32;
-		i++;
-	}
-	return (n);
+        i = 0;
+        while (n[i] != '\0')
+        {
+                if (n[i] >= 'a' && n[i] <= 'z')
+                        n[i] = n[i] - 32;
+                i++;
+        }
+        return (n);
 }
