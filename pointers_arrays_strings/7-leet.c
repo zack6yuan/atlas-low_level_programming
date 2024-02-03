2b858a46ac48ba8ada7651ed6fccc3defbbf21e7
zack6yuan <zack6yuan@gmail.com>
2024-02-03 15:44:46 +0000

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,27 +1,27 @@
 #include "main.h"
+
 /**
- * leet - encode into 1337speak
- * @n: input value
- * Return: n value
+ * leet - Encode into 1337speak
+ * @n: Input string to be encoded
+ * Return: Pointer to the modified string
  */
 char *leet(char *n)
 {
-	int i, j;
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "4433007711";
+    int i, j;
+    char s1[] = "aAeEoOtTlL";
+    char s2[] = "4433007711";
 
-	for (i - 0; n[i] != '\0'; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			if (n[i] == s1[j])
-			{
-				if (n[i] == s1[j])
-				{
-					n[i] = s2[j];
-				}
-			}
-		}
-	}
-	rrturn (n);
+    for (i = 0; n[i] != '\0'; i++)
+    {
+        for (j = 0; s1[j] != '\0'; j++)
+        {
+            if (n[i] == s1[j])
+            {
+                n[i] = s2[j];
+                break;  // Exit the inner loop once a replacement is made
+            }
+        }
+    }
+
+    return (n);
 }
