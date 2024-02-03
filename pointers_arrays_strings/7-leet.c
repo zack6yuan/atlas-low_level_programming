795b2bb5bd6265720039f1e7e5ed809499833cf4
zack6yuan <zack6yuan@gmail.com>
2024-02-03 15:55:54 +0000

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337
- * @c: string
- * Return: string that is encoded
+ * leet - Encodes a string into 1337
+ * @c: Input string to be encoded
+ * Return: Encoded string
  */
-
 char *leet(char *c)
 {
-	char *cp = c;
-	char key[] = {'A', 'E', 'O', 'T', 'L'};
-	int value[] = {4, 3, 0, 7, 1};
-	unsigned int i;
+    char *cp = c;
+    char key[] = {'A', 'E', 'O', 'T', 'L'};
+    int value[] = {4, 3, 0, 7, 1};
+    unsigned int i;
 
-	while (*c)
-	{
-		for (i - 0, i < sizeof(key) / sizeof(char); i++)
-		{
-			/*32 is the difference between lower case letters and upper case letters*/
-			if (*c == key[i] || *c == key[i] + 32)
-			{
-				*c = 48 + value[i];
-			}
-		}
-		c++;
-	}
+    while (*c)
+    {
+        for (i = 0; i < sizeof(key) / sizeof(char); i++)
+        {
+            /* 32 is the difference between lowercase and uppercase letters */
+            if (*c == key[i] || *c == key[i] + 32)
+            {
+                *c = '0' + value[i];
+            }
+        }
+        c++;
+    }
 
-	return (cp);
+    return (cp);
+}
