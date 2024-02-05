818550a7f18e41eb30734e47419a5b14551f2366
zack6yuan <zack6yuan@gmail.com>
2024-02-05 00:42:30 +0000

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,20 +2,23 @@
 #include "main.h"
 
 /**
- * _strchr - Entry point
- * @s: iput
- * @c: input
- * ReturnL Always 0 (Success)
+ * _strchr - Locates the first occurrence of a character in a string
+ * @s: Input string
+ * @c: Character to find
+ * Return: Pointer to the first occurrence of the character, or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+    int i;
 
-	for (i = 0; s[i] >= '\0'; i++)
-	{
-		if (s[i] == c)
-			return (s + i);
-	}
-	
-	return (NULL);
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == c)
+            return (s + i);
+    }
+
+    if (s[i] == c)
+        return (s + i);
+
+    return (NULL);
 }
