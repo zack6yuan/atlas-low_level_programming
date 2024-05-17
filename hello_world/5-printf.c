b93fbc3d5dc21530ea60c3630b77b4b1696e1d94
Zack Yuan <zack6yuan@gmail.com>
2024-05-17 02:56:54 -0500

    changes

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main (void)
 {
-    printf("Programming is like building a multilingual puzzle");
+    printf("with proper grammar, but the outcome is a piece of art,\n");
     return (0);
 }
