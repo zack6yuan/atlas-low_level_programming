cc14f2b46389665a7f6136180fa782c0b376a5a9
zack6yuan <zack6yuan@gmail.com>
2024-05-17 18:08:18 +0000

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+* main - prints the text "with proper grammer, but the outcome is a piece of art, followed by a new line"
+* Return: 0
+*/
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
+
     return (0);
 }
