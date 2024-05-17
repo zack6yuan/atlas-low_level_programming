22532af1d11612f42d22fcdd349084b741a38865
zack6yuan <zack6yuan@gmail.com>
2024-05-17 18:35:09 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+*main - prints the text required
+*Return: 0
+*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
+    puts("Programming is like building a multilingual puzzle\n");
+
     return (0);
 }
