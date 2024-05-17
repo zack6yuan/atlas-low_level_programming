6985faff122c8b3266a33e6fa373ac17e38d420a
zack6yuan <zack6yuan@gmail.com>
2024-05-17 18:59:17 +0000

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the text required
+ * 
+ * Return: 0
+*/
+ int main(void)
+ {
+    printf("the size of a char is:", sizeof(char));
+    printf("the size of an int it:", sizeof(int));
+    printf("the size of a long int:", sizeof(long int));
+    printf("the size of a float:", sizeof(float));
+
+    return (0);
+ }
