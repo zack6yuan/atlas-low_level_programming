57a2355dc469ce46d25b318e87effac626d653ce
zack6yuan <zack6yuan@gmail.com>
2024-05-19 22:18:54 +0000

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(void);
+int main(void)
 {
-write("and that piecec of art is useful"" - Dora Korpar, 2015-10-19\n")
+write("and that piecec of art is useful"" - Dora Korpar, 2015-10-19\n");
 
 return (1);
 }
