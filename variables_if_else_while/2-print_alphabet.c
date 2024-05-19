c1fffb1050aa3b17c37901cb660f88322bb5e501
zack6yuan <zack6yuan@gmail.com>
2024-05-19 05:59:48 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase, 
+ * main - prints the alphabet in lowercase,
  * followed by a new line
- * in the for loop, c is initialized as a, 
+ * in the for loop, c is initialized as a,
  * and moves on to the next letter using c++
  * as it reaches z, the for loop stops
  * Return: Always 0
