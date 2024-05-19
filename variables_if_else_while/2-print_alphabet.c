f21f424e905b50abad593daa93f1530c40747936
zack6yuan <zack6yuan@gmail.com>
2024-05-19 05:35:26 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,10 +2,17 @@
 
 /**
  * main - prints the alphabet in lowercase, followed by a new line
+ * in the for loop, c is initialized as a, and moves on to the next letter using c++
+ * as it reaches z, the for loop stops
  * Return: Always 0
  */
 
 int main(void)
 {
+    char c;
 
+    for (c = 'a'; c <= 'z'; c++) /*for loop*/
+    putchar(c);
+
+    return(0);
 }
