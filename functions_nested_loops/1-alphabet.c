60f4c0f302fe86ad71062a788cbf183e3ad03fb0
zack6yuan <zack6yuan@gmail.com>
2024-05-19 09:35:39 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line
+ * Return: 0
+*/
+void print_alphabet(void);
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return(0);
+}
