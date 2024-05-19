19e2f847a2cff58435d4fc2305aafe2fa194011d
zack6yuan <zack6yuan@gmail.com>
2024-05-19 22:21:12 +0000

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-fwrite("and that piecec of art is useful"" - Dora Korpar, 2015-10-19\n");
+fwrite("and that piece of art is useful"" - Dora Korpar, 2015-10-19\n");
 
 return (1);
 }
