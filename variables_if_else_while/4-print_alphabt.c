bbc8c06846354bda5269a1093d83837d4f83b0b0
zack6yuan <zack6yuan@gmail.com>
2024-05-19 06:39:06 +0000

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,10 @@ int main(void)
 char c;
 
 for (c = 'a'; c <= 'z'; c++)
-    {
-    if (c != 'e' && c != 'q') /* != means not equal to */
-        putchar (c);
-    }
+{
+if (c != 'e' && c != 'q') /* != means not equal to */
+putchar (c);
+}
 
 putchar('\n');
 
