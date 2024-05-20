b79c647df0e2ee4cbf5374c32f2b9d349d96b7ae
zack6yuan <zack6yuan@gmail.com>
2024-05-20 21:28:58 +0000

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c - function
+ */
+
+int _isalpha(int c);
+{
+if (C >= A && C <= Z;),(c >= a && c >= z)
+return(1);
+else
+return(0);
+}
