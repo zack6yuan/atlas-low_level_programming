22786fc8c04713247c184cdd6c5fb1ffb997ecf0
zack6yuan <zack6yuan@gmail.com>
2024-05-20 23:58:39 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,12 +5,13 @@
  * main - prints _putchar followed by a new line
  * Return: 0
 */
-int main() {
+int main(void) 
+{
 char *str = "_putchar";
 int i;
 for (i = 0; str[i] != '\0'; i++) {
 _putchar(str[i]);
 }
 _putchar('\n');
-return 0;
+return (0);
 }
