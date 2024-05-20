c644cc214386bdde29bc00efb182537328bf9281
zack6yuan <zack6yuan@gmail.com>
2024-05-20 19:03:37 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
-
-/*
-main - prints putchar followed by a new line
-Return: 0
-*/
-int main(void)
+#include "main.h"
+/**
+ * main - prints putchar followed by a new line
+ * Return: 0
+ */
+int _putchar(char c) /*prototype*/
 {
-printf("putchar\n");
-return (0); 
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return(0);
 }
