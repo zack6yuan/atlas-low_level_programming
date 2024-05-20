acb2da6b5dd414373293767b1076d97bb0e5fd90
zack6yuan <zack6yuan@gmail.com>
2024-05-20 22:25:00 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -12,6 +12,4 @@ for (c = 'a'; c <= 'z'; c++)
 putchar(c);
 }
 putchar('\n');
-
-return(0);
 }
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+void times_table(void)
+{
+int x, y;
+
+for (x = 0; x <= 10; i++)
+{
+for (y = 0; y <= 10; j++)
+{
+
+}
+}
+}
+}
+}
