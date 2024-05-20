6d6d4ccea18f50051b91ccd04139797e8e248139
zack6yuan <zack6yuan@gmail.com>
2024-05-20 23:08:29 +0000

    10-add.c

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -5,6 +5,7 @@
  * add - adds two integers and returns the result
  * @a: - the first integer that is being added
  * @b: - the second integer that is being added
+ * Return: The sum of the integers
 */
 
 int add(int a, int b)
