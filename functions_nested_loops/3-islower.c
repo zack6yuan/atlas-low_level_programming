2fd3dc1001bdd9112158743763c5f13e9b93147a
zack6yuan <zack6yuan@gmail.com>
2024-05-20 23:33:29 +0000

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -9,11 +9,11 @@
 int _islower(int c)
 {
 if ((c >= 'a') && (c <= 'z'))
-    {
-        return 1;
-    }
-        else
-    {
-        return 0;
-    }
+{
+    return 1;
+}
+    else
+{
+    return 0;
+}
 }
