ff1846e423bb71e69051292baba028bd4d0fd3b6
zack6yuan <zack6yuan@gmail.com>
2024-05-20 21:00:05 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
- * main - prints putchar followed by a new line
+ * main - prints _putchar followed by a new line
  * Return: 0
- */
-int main(void)
-{
-printf("_putchar\n");
-return(0);
+*/
+int main() {
+    char *str = "_putchar";
+    int i;
+    for (i = 0; str[i] != '\0'; i++) {
+        putchar(str[i]);
+    }
+    return 0;
 }
