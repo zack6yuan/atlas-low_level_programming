617485e4c4d371fc1da07db4d829c41b3c269df4
zack6yuan <zack6yuan@gmail.com>
2024-05-21 00:12:37 +0000

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,13 @@ void print_alphabetx10(void);
 {
 char c;
 int x = 0; 
-x++
 
-for (c = 'a'; c <= 'z'; c++)
+for (x = 0; x < 10; x++) /*the loop iterates ten times*/
+{
+for (c = 'a'; c <= 'z'; c++) /*a through z*/
 {
 _putchar(c);
 }
 _putchar('\n');
+}
 }
