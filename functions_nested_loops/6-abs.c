345d59fc104c7936620ef1c2ce731af5c49ab7ca
zack6yuan <zack6yuan@gmail.com>
2024-05-21 01:10:02 +0000

    6-abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -3,8 +3,8 @@
 
 /**
 *_abs - computes the absolute value of an integer
-@c: the value that is being checked
-Return: the absolute value of an integer
+* @c: the value that is being checked
+* Return: the absolute value of an integer
 */
 
 int _abs(int);
