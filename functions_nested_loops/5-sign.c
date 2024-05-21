e72fd038ed28d089ab7ecdc56ca75c2e6d00389f
zack6yuan <zack6yuan@gmail.com>
2024-05-21 00:40:39 +0000

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include "main.h"
 
-
-
-{}
+/**
+* print_sign - prints the sign of a number
+* @c: the number that is being checked
+* Return: 1 and + for > 0, 0 and 0 for = 0, and -1 and - for < 0
+*/
+int print_sign(int n);
+{
+    if (n > 0)
+    {
+    return (1);
+    }
+    else if (n = 0)
+    {
+    return (0);
+    }
+    else
+    {
+    return (-1);
+    }
+}
