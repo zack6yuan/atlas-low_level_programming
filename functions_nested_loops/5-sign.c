c7357c74fff6f4994e53f3fff7c2ac0a0b61f2da
zack6yuan <zack6yuan@gmail.com>
2024-05-21 00:42:33 +0000

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -6,13 +6,13 @@
 * @c: the number that is being checked
 * Return: 1 and + for > 0, 0 and 0 for = 0, and -1 and - for < 0
 */
-int print_sign(int n);
+int print_sign(int c)
 {
-    if (n > 0)
+    if (c > 0)
     {
     return (1);
     }
-    else if (n = 0)
+    else if (c = 0)
     {
     return (0);
     }
