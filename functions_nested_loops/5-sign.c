3bc29e3fa153877bcf04ba71041d35812540f58a
zack6yuan <zack6yuan@gmail.com>
2024-05-21 00:57:35 +0000

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int c)
 {
     if (c > 0)
     {
-    write("+");
+    fwrite("+");
     return (1);
     }
     else if (c == 0)
     {
-    write("0");
+    fwrite("0");
     return (0);
     }
     else
     {
-    write("-");
+    fwrite("-");
     return (-1);
     }
 }
