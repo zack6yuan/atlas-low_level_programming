1ee5c605bfee2db5ae4defb834bcaf802a689e4c
zack6yuan <zack6yuan@gmail.com>
2024-05-22 07:16:40 +0000

    commit message

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* print_numbers - prints numbers 0 to 9, followed by a new line
+* @n: initial number used
+* Return: 0
+*/
+
+void print_numbers(void)
+{
+    int n;
+    for (n = 0; n <= 9; n++)
+    putchar(n + '0');/*converts int to char to use _putchar*/
+    putchar('\n');
+}
