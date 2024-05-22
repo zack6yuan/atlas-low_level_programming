c581cd31a979e83554bfc78eac3c0e4108451788
zack6yuan <zack6yuan@gmail.com>
2024-05-22 06:11:57 +0000

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* _isupper - checks for uppercase character
+* c: initial parameter
+* Return: 1 if uppercase, 0 otherwise
+*/
+
+int _isupper(int c);
+{
+    if (c >= 65 && c <= 90)/*using ASCII values*/
+    return (1);
+    else
+    _isupper('\n');
+    return (0);
+}
