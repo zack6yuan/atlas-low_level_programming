15928521d0d276777759a3d35a400ab1dbf7273e
zack6yuan <zack6yuan@gmail.com>
2024-05-22 20:34:52 +0000

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers 0 to 14, ten times
+ * Return: 0
+*/
+
+void more_numbers(void)
+{
+    int a;
+    int b;
+    for (a = 0; a <= 14; a++) /*prints 0 to 14*/
+    {
+        for (b = 0; b <= 10; b++) /*nested loop iterates 10 times*/
+        printf("%d", a);  
+    }
+    printf('\n');   
+}
