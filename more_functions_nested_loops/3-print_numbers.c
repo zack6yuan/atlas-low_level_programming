5ffcc2cac80692493e7f191876a42281e42af09f
zack6yuan <zack6yuan@gmail.com>
2024-05-22 07:21:55 +0000

    3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -11,7 +11,7 @@ void print_numbers(void)
     int n;
     for (n = 0; n <= 9; n++)
     {
-    putchar(n + '0');/*converts int to char to use _putchar*/
+    putchar(n);/*converts int to char to use _putchar*/
     }
     putchar('\n');
 }
