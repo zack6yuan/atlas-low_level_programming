2b3d22c383177dec5bb32c16203a01127da6edfd
zack6yuan <zack6yuan@gmail.com>
2024-05-23 18:04:56 +0000

    changes

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,12 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
-* print_numbers - prints numbers 0 to 9, followed by a new line
+* print_numbers: prints numbers 0 to 9, followed by a new line
 * Return: void
 */
 
 void print_numbers(void)
 {
-int n;
-
-for (n = 0; n <= 9; n++)
-{
-_putchar(n + '0'); /*converts int to char*/
-}
-_putchar('\n');
+    
 }
