e55d9d32b6b17079c5fc7cab14e23e61ee532f90
zack6yuan <zack6yuan@gmail.com>
2024-05-23 21:34:16 +0000

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,28 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
 * main - prints the numbers 1 to 100
-* prints "Fizz" for multiples of 3 and "Buzz" for multiples of 5
-* For multiples of both, "FizzBuzz"
+* prints "Fizz" for multiples of 3 
+* prints "Buzz" for multiples of 5
+* prints "FizzBuzz" for multiples of 3 and 5
 * Return: 0
 */
 
 int main()
 {
-    
+int n; 
+for (n = 1; n <= 100; n++) /*prints 1 to 100*/
+{
+if ((n % 3) == 0) /*condition 1*/
+printf("Fizz");
+else if ((n % 5) == 0) /*condition 2*/
+printf("Buzz");
+else if ((n % 3) == 0) && ((n % 5) == 0) /*condition 3*/
+printf("FizzBuzz");
+else /*condition 4*/
+printf("%d",n); 
+}
+printf('\n');
+return (0);
 }
