d19dd84677d1a2b2a02c6cd36ce6779cdb2f614c
zack6yuan <zack6yuan@gmail.com>
2024-05-23 20:16:43 +0000

    9-fizz-buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the numbers 1 to 100
+* prints "Fizz" for multiples of 3 and "Buzz" for multiples of 5
+* For multiples of both, "FizzBuzz"
+* Return: 0
+*/
+
+int main()
+{
+    
+}
