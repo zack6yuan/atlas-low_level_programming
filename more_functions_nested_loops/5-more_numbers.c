ca3a4669505b7cfb7e1ca556acd3c26a01f191df
zack6yuan <zack6yuan@gmail.com>
2024-05-23 18:18:05 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,12 @@
 
 void more_numbers(void)
 {
-    int a;
-    int b;
-    for (a = 0; a <= 14; a++) /*prints 0 to 14*/
-    {
-        for (b = 0; b <= 10; b++) /*nested loop iterates 10 times*/
-        putchar(a + '0');  
-    }
-    putchar('\n');   
+int a;
+int b;
+for (a = 0; a <= 14; a++) /*prints 0 to 14*/
+{
+for (b = 0; b <= 10; b++) /*nested loop iterates 10 times*/
+_putchar(a + '0');  
+}
+_putchar('\n');   
 }
