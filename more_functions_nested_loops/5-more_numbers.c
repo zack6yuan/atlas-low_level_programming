2d42f190da3b7ab36cbe527bdb4a01c3b542aa20
zack6yuan <zack6yuan@gmail.com>
2024-05-24 23:27:15 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,15 @@
 void more_numbers(void)
 {
 int a;
-int b = 0; /*reset b to 0*/
+int b = 0; /*set b to 0*/
 for (a = 0; a < 10; a++)/*outer loop controls inner loop*/
 {
 for (b = 0; b <= 14; b++) /*0 to 14*/
+{
+if (b > 9)/*to print numbers with two digits*/
 _putchar(b / 10 + '0'); /*tens digit*/
 _putchar(b % 10 + '0'); /*ones digit*/
 }
 _putchar('\n');
+}
 }
