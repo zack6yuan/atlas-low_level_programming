3b22657f228289e663289e263839e72094c209c4
zack6yuan <zack6yuan@gmail.com>
2024-05-24 06:15:54 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,6 +2,7 @@
 
 /**
  * more_numbers - prints the numbers 0 to 14, ten times
+
  * Return: void
 */
 
@@ -9,9 +10,9 @@ void more_numbers(void)
 {
 int a;
 int b;
-for (a = 0; a <= 14; a++) /*prints 0 to 14*/
+for (a = 0; a < 10; a++)/*outer loop controls inner loop*/
 {
-    for (b = 0; b <= 10; b++) /*nested loop iterates 10 times*/
+    for (b = 0; b <= 14; b++) /*prints 0 to 14*/
 _putchar(a + '0');  
 }
 _putchar('\n');   
