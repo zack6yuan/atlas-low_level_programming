a18d013d9b44282aa72587909e09a712e4a23764
zack6yuan <zack6yuan@gmail.com>
2024-05-25 00:04:38 +0000

    8-main.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_square - prints a square, followed by a new line
+* @size: the size of the square, length and width
+* Return: void
+*/
+
+void print_square(int size)
+{
+if (size <= 0)
+{
+_putchar('\n'); /*new line if n <= 0*/
+}
+else
+{
+int a;
+int b;
+for (a = 0; a < n; a++) /*for loop for width*/
+{
+for (b = 0; b < a; b++) /*for loop for length*/
+{
+_putchar('#'); /*prints the # character*/
+}
+_putchar('\n');
+}
+}
+}
