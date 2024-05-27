3d296a3f91b2aa2626dacbd0d74a11ec3ef93368
zack6yuan <zack6yuan@gmail.com>
2024-05-27 05:07:39 +0000

    1-swap.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/*
+* _puts - a function that prints a string, followed by a new line
+* Return: void
+*/
+void _puts(char *str)
+{
+
+}
---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string, in reverse
+ * followed by a new line
+ * @s: the input for the string
+*/
+
+void print_rev(char *s)
+{
+
+}
