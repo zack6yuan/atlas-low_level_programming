166b29f01855b275d9ea79d28ef7b893eed8e1a9
zack6yuan <zack6yuan@gmail.com>
2024-05-27 05:06:26 +0000

    1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -10,5 +10,10 @@
 
 void swap_int(int *a, int *b)
 {
-
+int x;
+{
+x = *a; /*x is assigned to the pointer of a*/
+*a = *b; /*the pointer a is assigned to the pointer b*/
+*b = x;/*the pointer b is assigned back to x, switching them*/
+}
 }
