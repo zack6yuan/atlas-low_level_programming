12854d6199f5b2742b9aac5f77cbea37c8db04c3
zack6yuan <zack6yuan@gmail.com>
2024-05-27 14:22:13 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -4,9 +4,17 @@
  * print_rev - a function that prints a string, in reverse
  * followed by a new line
  * @s: the input for the string
+ * Return: void
 */
 
 void print_rev(char *s)
 {
-
+int x;
+int length = 0;
+for (x = 0; s[x] != '\0'; x++) /*for loop, runs until reaching null*/
+length++;
+for (x = length -1; x >= 0; x--) /*iterates in reverse*/
+_putchar (str[x]);
+}
+_putchar ('\n');
 }
