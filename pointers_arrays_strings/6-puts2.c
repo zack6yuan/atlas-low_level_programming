62dfd2a4acb76239b00c638e1e1f3f4f50147778
zack6yuan <zack6yuan@gmail.com>
2024-05-27 15:00:11 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @s: the pointer
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+int x;
+for (x = 0; str[x] != '\0'; x++) /*length of the string*/
+{
+if (x % 2 == 0) /*checking for even numbers*/
+{
+_putchar(str[x]);
+}
+}
+_putchar('\n');
+}
