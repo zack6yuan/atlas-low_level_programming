0d6fcc9baf71248c51ae781abf7c10a72dfe3010
zack6yuan <zack6yuan@gmail.com>
2024-05-27 14:00:47 +0000

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
 /*
-* _puts - a function that prints a string, followed by a new line
-* Return: void
-*/
+ * _puts - a function that prints a string, followed by a new line
+ * @str: the string
+ * Return: void
+ */
 void _puts(char *str)
 {
-
+int x;
+for (x = 0; str[x] != '\0'; x++) /*for loop, runs until reaching null*/
+{
+_putchar (str[x]); /*prints the string*/
+}
+_putchar ('\n');
 }
