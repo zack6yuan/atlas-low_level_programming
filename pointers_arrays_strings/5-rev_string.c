91106fc5decb58248e55f6a6f1b3c6637e730750
zack6yuan <zack6yuan@gmail.com>
2024-05-28 03:02:07 +0000

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -2,16 +2,23 @@
 #include <stdio.h>
 
 /**
- * rev_string - reverses a string
- * @s: the string
- * Return: void 
+ * rev_string - reverses a string and prints it
+ * @s: the string's pointer
+ * Return: 0
  */
 
 void rev_string(char *s)
 {
+char c;
 int x;
-int length = 0;
+int digits = 0;
 
-for (x = 0; s[x] != '\0'; x++)
-length++;
+for (digits = 0; s[digits] != '\0'; digits++) /*finds the length of the string, until reaches null*/
+{
+for (x = 0; x < digits; x++) /*half of the string*/
+while (digits--) 
+c = s[x]; /*stores int x in char c*/
+s[x] = s[digits]; /*stores int digits in int and swaps*/
+s[digits] = c; /*finishes the switch*/
+}
 }
