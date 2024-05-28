edfca877e199496d7000e8448a0a9bb88283a3bc
zack6yuan <zack6yuan@gmail.com>
2024-05-28 01:29:23 +0000

    9-strcpy.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _atoi - a function that converts a string to an integer
+* @s: converted string
+* Return: 0
+*/
+
+int _atoi(char *s)
+{
+
+}
--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,10 +4,16 @@
 /**
 * _strcpy - copies the string pointed to by src, including the
 * terminating null byte (/0), to the buffer pointed to by dest
-* @src:
-* @dest:
+* @src: where we are copying from
+* @dest: where we are copying to
 * Return: 0
 */
 
 char *_strcpy(char *dest, char *src)
-{}
+{
+int x;
+for (x = 0; src[x] != '\0'; x++)
+dest[x] = src[x]; /*copies from src to dest*/
+dest[x] = '\0'; /*ends the interation, completed
+return (dest); /*the pointer to dest*/
+}
