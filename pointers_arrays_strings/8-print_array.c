96b19691e85e91ed1ee46221189945846b43b7d8
zack6yuan <zack6yuan@gmail.com>
2024-05-28 01:04:53 +0000

    changes

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_array - prints n elements of an array of integers
+* followed by a new line
+* @a: the first integer
+* @n: the second integer
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+
+}
---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strcpy - copies the string pointed to by src, including the
+* terminating null byte (/0), to the buffer pointed to by dest
+* @src:
+* @dest:
+* Return: 0
+*/
+
+char *_strcpy(char *dest, char *src)
+{}
