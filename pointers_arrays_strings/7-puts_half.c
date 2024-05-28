19bb4ac8ebb0ebb10e0a3b10381705d2794557a8
zack6yuan <zack6yuan@gmail.com>
2024-05-28 01:04:52 +0000

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string, and a new line
+ * @str: string
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+int x; 
+int y; /*used to get the second half of the string*/
+int length;
+
+for (x = 0; str[x] != '\0'; x++) /*finds the length of the string*/
+length++;
+y = length / 2; 
+if (length % 2 != 0) /*used to round up if the length is odd*/
+{
+y++; 
+}
+for (x = y; str[x] != '\0'; x++)
+_putchar(str[x]);
+_putchar('\n');
+}
