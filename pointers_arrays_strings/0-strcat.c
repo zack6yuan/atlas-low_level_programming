c588b56566e4f03d5e72a80634d4bf0e0f4b44a4
zack6yuan <zack6yuan@gmail.com>
2024-05-29 19:57:08 +0000

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -5,20 +5,18 @@
  * _strcat - concatenates two strings
  * @src: the source
  * @dest: the destination
- * Return: the concatenated strings
+ * Return: dest pointer
  */
 
 char *_strcat(char *dest, char *src)
 {
-    int x = 0;
-    int y = 0;
+    int x;
+    int y;
     for (x = 0; dest[x] != '\0'; x++) /*length of dest*/
     for (y = 0; src[y] != '\0'; y++) /*length of src*/
     {
-    dest[x + y] = src[y];
+        dest[x] = src[y];
     {
-    dest[x + y] = '\0';
-    }
-    }
-    return (dest);
+        dest[x] = '\0';
+        return (dest);
 }
