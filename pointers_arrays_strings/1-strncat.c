49522dbcae2dc98059379e4b174e7c40e95b3798
zack6yuan <zack6yuan@gmail.com>
2024-05-29 20:54:02 +0000

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -3,12 +3,26 @@
 
 /**
  * _strncar - concatenates two strings
- * @dest:
- * @src:
+ * @dest: destination
+ * @src: source
+ * @n: the max number of bytes used
  * Return: the concatenated strings
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-
+int x = 0;
+int y = 0;
+while (dest[x] != '\0') /*length of dest*/
+{
+    x++;
+}
+while (src[y] != '\0' && y < n)
+{
+    dest[x] = src[y];
+    x++;
+    y++;
+}
+dest[x] = '\0';
+return (dest);
 }
