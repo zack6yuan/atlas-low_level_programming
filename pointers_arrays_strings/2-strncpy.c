db1169452496a33f22153d93105ee04e41a4ecc6
zack6yuan <zack6yuan@gmail.com>
2024-05-29 21:05:55 +0000

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -3,12 +3,25 @@
 
 /**
  * _strncpy - copies a string
- * @dest:
- * @src:
+ * @dest: destination
+ * @src: source
+ * @n: the input value
  * Return: the copied string
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
+int y = 0;
 
+while (src[y] != '\0' && y < n)
+{
+dest[y] = src[y]; /*to copy*/
+y++;
+}
+while (y < n) /*parameter for at most n bytes*/
+{
+dest[y] = '\0';
+y++;
+}
+return (dest);
 }
