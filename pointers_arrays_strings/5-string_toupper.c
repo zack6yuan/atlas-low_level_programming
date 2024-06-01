0d5b07b110b783cad8df1d7ee815e053c1804245
zack6yuan <zack6yuan@gmail.com>
2024-06-01 03:07:34 +0000

    commit message

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,11 +2,21 @@
 #include <stdio.h>
 
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase
- * Return: 0
+ * string_toupper - changes all lowercase letters 
+ * of a string to uppercase using ASCII
+ * @s: being converted
+ * Return: new string
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
-    
+int x;
+
+while (s[x] != '\0')
+{
+if (s[x] >= 97 && s[x] <= 122) /* 97 (a) and 122 (z) */
+s[x] = s[x] - 32; /*lowercase to uppercase using ASCII*/
+x++; 
+}
+return (0);
 }
