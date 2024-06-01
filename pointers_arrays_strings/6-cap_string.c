71e6918c1ec04922a635e2cca6422a3451d56f0d
zack6yuan <zack6yuan@gmail.com>
2024-06-01 19:41:12 +0000

    cap string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,33 @@
  * Return: the capitalized words of the string
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
+int x = 0;
 
+while (s[x])
+{
+while (!(s[x] >= 'a' && s[x] <= 'z')) /*while not lowercase*/
+x++;
+
+if (s[x - 1] == ' ' || /*space*/
+s[x - 1] == '\t' || /*tabulation*/
+s[x - 1] == '\n' || /*new line*/
+s[x - 1] == ',' ||
+s[x - 1] == ';' ||
+s[x - 1] == '.' ||
+s[x - 1] == '!' ||
+s[x - 1] == '?' ||
+s[x - 1] == '"' ||
+s[x - 1] == '(' ||
+s[x - 1] == ')' ||
+s[x - 1] == '{' ||
+s[x - 1] == '}' )
+{
+if (s[x] >= 'a' && s[x] <= 'z')
+s[x] = s[x] - 32; /*lowercase to uppercase using ASCII*/
+}
+x++;
+}
+return (s);
 }
