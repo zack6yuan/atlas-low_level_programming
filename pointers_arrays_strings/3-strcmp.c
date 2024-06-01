1357c96ddf8c324f548e39c83aafd508d43fdca3
zack6yuan <zack6yuan@gmail.com>
2024-06-01 01:03:54 +0000

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -3,10 +3,21 @@
 
 /**
  * _strcmp - compares two strings
- * Return: 0
+ * @s1: the first compared string
+ * @s2: the second compared string
+ * Return: the result of the compared strings
  */
 
 int _strcmp(char *s1, char *s2)
 {
-
+int x;
+while (s1[x] != '\0' && s2[x] != '\0')
+{
+if (s1[x] != s2[x]) /*condition*/
+{
+return (s2[x] - s1[x]); 
+}
+x++ /*iteration*/
+}
+return (0);
 }
