876596c93ecacc6b71878f639f1df163e04ea56b
zack6yuan <zack6yuan@gmail.com>
2024-06-02 18:20:31 +0000

    commit message

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination of copied memory
+ * @src: original location
+ * @n: number of bytes
+ * Return: pointer to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int x = 0; 
+
+while (x < n) /*bytes*/
+{
+dest[x] = src[x]; /*copies from src to dest*/
+x++;
+}
+return (dest); /*returned pointer*/
+}
