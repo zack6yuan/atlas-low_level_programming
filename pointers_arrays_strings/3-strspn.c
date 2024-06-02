6af1c4af341380ff6fbd625d886a7c216215ab0f
zack6yuan <zack6yuan@gmail.com>
2024-06-02 18:42:36 +0000

    3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: initial segment
+ * @accept: parameter
+ * Return: length of the substring
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
