a7d7efbd90ff6894b2c100c932ffb646e2ab2e17
zack6yuan <zack6yuan@gmail.com>
2024-06-03 15:50:58 +0000

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,4 +10,19 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
+unsigned int length = 0;
+unsigned int x;
+unsigned int y;
+for (x = 0; s[x] != '\0'; x++) /*iterates through s*/
+{
+    for (y = 0; accept[y] != '\0'; y++) /*iterates through accept*/
+    {     
+        if (s[x] == accept[y])
+        {
+            length++;
+            break;
+        }
+    }
+}
+return (length);
 }
