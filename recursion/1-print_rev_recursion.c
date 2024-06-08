2ffcb2f9a443fd426ce3bafd56671cca157565ae
zack6yuan <zack6yuan@gmail.com>
2024-06-08 23:23:39 +0000

    *s before recursive

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -8,12 +8,10 @@
 void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
-{
 _putchar('\n'); /*new line if null*/
 }
 else
 {
 _print_rev_recursion(s + 1); /*recursive call first for reverse*/
 _putchar(*s); /*prints string*/
-}
 }
