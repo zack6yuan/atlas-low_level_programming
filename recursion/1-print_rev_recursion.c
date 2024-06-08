61e11eafaaed6bb1f5728b93d0b1677598bbcbed
zack6yuan <zack6yuan@gmail.com>
2024-06-08 23:27:22 +0000

    didn't fully read directions

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,11 +7,7 @@
  */
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
-{
-_putchar('\n'); /*new line if null*/
-}
-else
+if (*s != '\0')
 {
 _print_rev_recursion(s + 1); /*recursive call first for reverse*/
 _putchar(*s); /*prints string*/
