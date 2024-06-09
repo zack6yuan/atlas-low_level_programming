b77ac09053d78065a562ecb8897dc1908d62c5e4
zack6yuan <zack6yuan@gmail.com>
2024-06-09 00:41:07 +0000

    3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -3,9 +3,17 @@
 /**
  * factorial - returns the factorial of a given number
  * @n: the given number
- * Return: the factorial of the given number
+ * Return: -1 if  n < 0
  */
 int factorial(int n)
 {
-
+if ((n == 0) || (n == 1))
+{
+return (1); /*returns factorial of 0 or 1*/
+}
+if (n < 0)
+{
+return (-1); /*error indicated*/
+}
+return (n * factorial(n - 1)); /*number multiplied by recursive for factorial*/
 }
