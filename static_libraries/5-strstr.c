5e79e010a44735c6f491de3679e20995d55ba727
zack6yuan <zack6yuan@gmail.com>
2024-06-09 23:54:40 +0000

    commit message

--- static_libraries/5-strstr.c
+++ static_libraries/5-strstr.c
@@ -9,14 +9,24 @@
 */
 
 char *_strstr(char *haystack, char *needle)
-
-int x; /*variable for haystack*/
-int y; /*variable for needle*/
-
-if (needle == '\0') /*if needle is not found, null*/
 {
+int x;
+if (*needle == 0)
 return (haystack);
-}
-while (x = 0; haystack[x] != '\0'; x++)
+while (*haystack)
 {
-while (y = 0; needle[y] != '\0'; y++)
+x = 0;
+if (haystack[x] == needle[x])
+{
+do
+{
+if (needle[x + 1] == '\0')
+return (haystack);
+x++;
+}
+while(haystack[x] == needle[x]);
+}
+haystack++;
+}
+return(NULL);
+}
