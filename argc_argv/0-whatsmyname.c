6d2528350d2d2ecdd98ebec391efaa38c9438e50
zack6yuan <zack6yuan@gmail.com>
2024-06-09 17:40:17 +0000

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name
+ * @argc: count of arguments
+ * @argv: vector of arguments 
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]); /*prints the program name*/
+return (0);
+}
