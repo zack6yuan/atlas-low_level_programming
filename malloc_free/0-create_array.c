a15013667157131998ef4c8aba0e70721b2a0947
zack6yuan <zack6yuan@gmail.com>
2024-06-13 14:07:19 +0000

    betty format

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,30 +2,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * create_array - creates an array of chars, and initializes it with a specific char
+ * create_array - creates an array of chars, initializes with specific char
  * @size: the size of the array
  * @c: specific char initialized
  * Return: pointer to the array, or NULL if it fails
  */
 char *create_array(unsigned int size, char c)
 {
-    char *array; /*pointer to array*/
-    unsigned int x; /*used to iterate through array*/
+char *array; /*pointer to array*/
+unsigned int x; /*used to iterate through array*/
 
-    if (size == 0) /*NULL if size == 0*/
-    {
-        return (NULL);
-    }
+if (size == 0) /*NULL if size == 0*/
+{
+return (NULL);
+}
 
-    array = malloc(sizeof(char) * size);
+array = malloc(sizeof(char) * size);
 
-    if (array == NULL) /*if NULL, return NULL*/
-    {
-        return (NULL);
-    }
-    for (x = 0; x < size; x++) /*iteration through array*/
-    {
-        array[x] = c;
-    }
-    return(array); /*return pointer*/
+if (array == NULL) /*if NULL, return NULL*/
+{
+return (NULL);
+}
+for (x = 0; x < size; x++) /*iteration through array*/
+{
+array[x] = c;
+}
+return(array); /*return pointer*/
 }
