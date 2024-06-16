aae96f30fdc3f4a5c13b4a60825e0a6f1432c15d
zack6yuan <zack6yuan@gmail.com>
2024-06-16 03:49:03 +0000

    file creation

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * malloc_checked - function that allocates using malloc
+ * returns a pointer to the allocated memory
+ * @b: 
+ * Return: 
+ * /
