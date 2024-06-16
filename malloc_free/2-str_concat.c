b7439dec4b309c15a0665c4464bc8c47ca23bbd8
zack6yuan <zack6yuan@gmail.com>
2024-06-16 16:46:55 +0000

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,12 +9,28 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-char *ptr;
-int x;
-int y;
+char *pointer;
+int x = 0; /*iteration thorough s1*/
+int y = 0; /*iteration thorough s2*/
+int size1 = 0; /*size of s1*/
+int size2 = 0; /*size of s2*/
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+while (s1[size1] != '\0') /*while not null, iterate*/
+size1++; 
+while (s2[size2] != '\0') /*while not null, iterate*/
+size2++;; 
+pointer = malloc(sizeof(char)*(size1 + size2 + 1)); /*concat strings + space for NULL*/
+if (pointer == NULL)
+return (NULL);
+
+for (x = 0; x < size1; x++)
+{
+pointer[x] = s1[x]; /*copy into pointer*/
+}
+for (y = 0; y < size2; y++)
+{
+pointer[x + y] = s2[y];
+}
+pointer[x + y] = '\0'; /*null terminate*/
+return (pointer); /*return pointer*/
 }
