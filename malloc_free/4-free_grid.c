d652dd86bfff0de0e4ba31da76a42fb59fa36b25
zack6yuan <zack6yuan@gmail.com>
2024-06-16 16:47:00 +0000

    commit message

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,5 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * free_grid - frees a 2 dimensional grid previously created by alloc_grid 
+ * free_grid - frees a 2 dimensional grid previously created by alloc_grid
+ *  
  */
