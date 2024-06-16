41c1f20d2103610668b905511c0d25861a67d3bb
zack6yuan <zack6yuan@gmail.com>
2024-06-16 04:00:47 +0000

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -5,6 +5,16 @@
 /**
  * malloc_checked - function that allocates using malloc
  * returns a pointer to the allocated memory
- * @b: 
- * Return: 
- * /
+ * @b: unsigned int
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b);
+{
+int *pointer; /*stores the memory*/
+pointer = malloc(b); /*malloc statement*/
+if (pointer == NULL)
+{
+exit(98); /*exit status value*/
+}
+return (pointer); /*pointer returned to allocated memory*/
+}
