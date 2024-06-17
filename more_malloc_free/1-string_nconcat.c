b1230c220c65136ba541705b592e5cf2c618cc9a
zack6yuan <zack6yuan@gmail.com>
2024-06-17 04:55:57 +0000

    commit message

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,40 +10,40 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    int x = 0;
-    int y = 0;
-    int length;
-    char *pointer;
+int x = 0;
+int y = 0;
+int length;
+char *pointer;
 
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
 
-    while (s1[x]) /*calculate length*/
-        x++;
-    while (s2[y]) /*calculate length*/
-        y++;
+while (s1[x]) /*calculate length*/
+x++;
+while (s2[y]) /*calculate length*/
+y++;
 
-    length = x + y;
+length = x + y;
 
-    pointer = malloc(sizeof(char) * length + 1); /*malloc statement*/
+pointer = malloc(sizeof(char) * length + 1); /*malloc statement*/
 
-    if (pointer == NULL)
-        return (NULL);
+if (pointer == NULL)
+return (NULL);
 
-    for (x = 0; x < length; x++) /*calculate length*/
-    {
-    if ((unsigned int)x < n)
-        pointer[x] = s1[x];
-    else
-        pointer[x] = s2[x - n];
-    }
-    pointer[length] = '\0'; /*null terminate*/
+for (x = 0; x < length; x++) /*calculate length*/
+{
+if ((unsigned int)x < n)
+pointer[x] = s1[x];
+else
+pointer[x] = s2[x - n];
+}
+pointer[length] = '\0'; /*null terminate*/
     
-    return (pointer); /*return pointer*/
+return (pointer); /*return pointer*/
 }
