4a6ebe8377e6fdeb50788628330417df4b7ced74
zack6yuan <zack6yuan@gmail.com>
2024-06-19 21:58:37 +0000

    commit message

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * main - defines struct dog with elements
+ * @dog: struct (dog_)
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: void
+ */
+struct dog
+{
+    char *name;
+    char *owner;
+    float *age;
+};
+
+int main(void);
+{
+    struct Dog dog;
+    dog->name = name;
+    dog->age = age;
+    dog->owner = owner;
+    return dog;
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,19 @@
 #ifndef dog_h
 #define dog_h
+
+/**
+ * struct dog - struct for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+  */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
