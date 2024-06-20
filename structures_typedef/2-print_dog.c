de0dcc023f1cac6dc9b0ea5b01beae9d9e7d1eb2
zack6yuan <zack6yuan@gmail.com>
2024-06-20 17:01:58 +0000

    betty

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 if (d == NULL)
 return; /*return nothing*/
 /*using "?", if != NULL, prints value, otherwise prints "nil"*/
-printf("Name: %s\n", d ->name ? d->name : "(nil)"); /*string format*/
+printf("Name: %s\n", d->name ? d->name : "(nil)"); /*string format*/
 printf("Age: %f\n", d->age); /*float format*/
 printf("Owner: %s\n", d->owner ? d->owner : "(nil)"); /*string format*/
 }
