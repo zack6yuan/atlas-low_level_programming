13bf5a50ce837d8da36ff944176f0497be4e7eba
zack6yuan <zack6yuan@gmail.com>
2024-06-20 17:05:44 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,11 @@
 #ifndef dog_h
 #define dog_h
-
 /**
  * struct dog - struct for dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ * Return: void
   */
 struct dog
 {
@@ -13,10 +13,9 @@ char *name;
 float age;
 char *owner;
 };
-
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dgo dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
