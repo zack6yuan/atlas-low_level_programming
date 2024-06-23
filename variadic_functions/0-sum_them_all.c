eeeff2cdc6e05239e0800de6f208dd0e84ab0b50
zack6yuan <zack6yuan@gmail.com>
2024-06-23 21:12:39 +0000

    commit message

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * Return: the sum
+ */
+int sum_them_all(const unsigned int n, ...);
+{
+
+}
