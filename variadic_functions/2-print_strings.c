dace00ad63baf341fee418e5ac6954e3631692cb
zack6yuan <zack6yuan@gmail.com>
2024-06-24 21:03:59 +0000

    2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
  * print_strings - prints strings, followed by a new line
@@ -8,5 +9,29 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+unsigned int x;
+char *string;
 
+va_list digits;
+va_start(digits, n);
+
+for (x = 0; x < n; x++)
+{
+string = va_arg(digits, char *);
+if (string == NULL) /*parameter*/
+{
+string = "nil";
+}
+else
+{
+printf("%s", string); /*print string value*/
+}
+
+if (x < n - 1 && separator != NULL)
+{
+printf("%s", separator); /*print the seperator*/
+}
+}
+va_end(digits);
+printf("\n"); /*newline character*/
 }
