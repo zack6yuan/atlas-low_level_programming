b8e6f3689e9f2c750dc1498c754274dec118bbb7
zack6yuan <zack6yuan@gmail.com>
2024-06-24 20:41:07 +0000

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
  * print_numbers - prints numbers, followed by a new line
@@ -8,5 +9,18 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+va_list digits;
+unsigned int x;
 
+va_start(digits, n);
+for (x = 0; x < n; x++)
+{
+printf("%d", va_arg(digits, int));
+if (x != (n - 1) && separator != NULL)
+{
+printf("%s", separator); /*prints the seperator*/
+}
+}
+va_end(digits);
+printf("\n"); /*newline character*/
 }
