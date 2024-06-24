25c604eae3e85647fcb41a4408645a100b34fb22
zack6yuan <zack6yuan@gmail.com>
2024-06-24 17:26:07 +0000

    defined "n"

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,6 +2,7 @@
 #include "variadic_functions.h"
 /**
  * sum_them_all - returns the sum of all its parameters
+ * @n: the number of parameters
  * Return: the sum
  */
 int sum_them_all(const unsigned int n, ...)
