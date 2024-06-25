24934635b26d6572ed819d1f1c6847972c57d1fc
zack6yuan <zack6yuan@gmail.com>
2024-06-25 03:01:27 +0000

    commit message

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - returns the sum of a + b
+ * @a: the first number being added
+ * @b: the second number being added
+ * Return: value of added numbers
+ */
+int op_add(int a, int b);
+{
+return (a + b);
+}
+/**
+ * op_sub - returns the difference of a - b
+ * @a: number that will be subtracted from
+ * @b: tnumber subtracted from the first number
+ * Return: value of subtracted numbers
+ */
+int op_sub(int a, int b);
+{
+return (a - b);
+}
+/**
+ * op_mul - returns the multiplication of a * b
+ * @a: first number that is being multiplied
+ * @b: second number that is beign multiplied
+ * Return: value of multiplied numbers
+ */
+int op_mul(int a, int b);
+{
+return (a * b);
+}
+/**
+ * op_div - returns the divison of a % b
+ * @a: number that will be divided
+ * @b: number that the first number will be divided by
+ * Return: value of divided numbers
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - returns the remainder of division of a / b
+ * @a: the number that will be divided
+ * @b: number that the first number will be divided by
+ * Return: the remainder or modulo
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * new_dog - creates a new dog and stores a copy of name and owner
@@ -9,5 +10,29 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+dog_t *puppy;
+int x = 0
+int y = 0, z;
 
+for (x = 0; name[x] != '\0'; x++) /*loop for dog's name*/
+for (y = 0; owner[y] != '\0'; y++) /*loop for dog's owner*/
+
+puppy = malloc(sizeof(dog_t));
+if (puppy == NULL)
+{
+free(puppy);
+return (NULL);
 }
+puppy->name = malloc(x * sizeof(char));
+puppy->owner = malloc(y * sizeof(char));
+
+if (puppy->name == NULL)
+{
+free(puppy->name);
+}
+if (puppy->owner == NULL)
+{
+free(puppy->owner);
+}
+free(puppy);
+return (NULL);
