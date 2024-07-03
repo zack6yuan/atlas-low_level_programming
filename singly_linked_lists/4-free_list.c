391b5490fbe87f5554b880e17557fbe6efe7d63c
zack6yuan <zack6yuan@gmail.com>
2024-07-03 04:02:39 +0000

    file creation

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head of the list_t list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	
