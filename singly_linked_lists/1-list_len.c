68e522e8d38385a55d931d6f23e0222b90cf046a
zack6yuan <zack6yuan@gmail.com>
2024-07-03 03:23:53 +0000

    1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: list pointer
+ * Return: the number of elements in the linked list
+ */
+
+size_t list_len(const list_t *h);
+{
+	int counter = 0; /*counter for elements*/
+
+	while (h != NULL)
+	{
+		count++; /*increment the counter*/
+		h = h->next; /*move to next node*/	
+	}
+	return (count);
+}
+
