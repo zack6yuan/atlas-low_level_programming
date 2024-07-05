6ead18aed0fa8fc8b5d7a532e9f22f1d855c934b
zack6yuan <zack6yuan@gmail.com>
2024-07-05 19:30:53 +0000

    first question done

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -4,10 +4,19 @@
 #include "lists.h"
 /**
  * print_listint - prints all the elements of a listint_t list
- * @h:
+ * @h: head of the linked list listint_t
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
+    size_t nodes = 0; /*counter for nodes*/
 
+    while (h != NULL)
+    {
+        printf("%d\n", h->n); /*value of current node*/
+        h = h->next; /*move to the next node*/
+        nodes++; /*increment the counter*/
+    }
+
+    return (nodes); /*return node count*/
 }
