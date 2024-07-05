d8bf96faf2e92e86503f868acc9f75e44e6abee7
zack6yuan <zack6yuan@gmail.com>
2024-07-05 20:00:45 +0000

    question 2

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -5,10 +5,24 @@
 /**
  * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head: the head of the listint_list
- * @n: 
+ * @n: new value to be created
  * Return: the address of the new element, or NULL if failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+listint_t *node
+if (head == NULL) /*check if empty*/
+{
+return (NULL);
+}
+node = malloc(sizeof(listint_t));
+if (node == NULL)
+{
+return (NULL);
+}
+node->n= n; /*assign the value of the new node*/
+node->next = *head; /*set the new node to head*/
+*head = node; /*set the head to the new node*/
 
+return (node); /*return address of the new element*/
 }
