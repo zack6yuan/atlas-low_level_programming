d79ce333f633bb243b2a1bfc7a7eac6c37a41779
zack6yuan <zack6yuan@gmail.com>
2024-07-05 19:45:06 +0000

    forgot documentation

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 /**
  * listint_len - returns the number of elements in a linked listint_t list
- * @h:
+ * @h: head of the linked list listint_t
  * Return: the number of elements in the linked list
  */
 size_t listint_len(const listint_t *h)
