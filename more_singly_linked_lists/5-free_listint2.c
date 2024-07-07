cd12c1b0fc6ad415767d4f687ab8a5457f9ab9a8
zack6yuan <zack6yuan@gmail.com>
2024-07-07 18:59:17 +0000

    betty

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -6,17 +6,17 @@
  */
 void free_listint2(listint_t **head)
 {
-    listint_t *node;
+listint_t *node;
 
-    if (head == NULL) /*parameter*/
-    {
-        return;
-    }
-    while (*head != NULL)
-    {
-    node = *head;
-    *head = (*head)->next;
-    free(node);
-    }
-    *head = NULL; /*head is set to NULL*/
+if (head == NULL) /*parameter*/
+{
+return;
+}
+while (*head != NULL)
+{
+node = *head;
+*head = (*head)->next;
+free(node);
+}
+*head = NULL; /*head is set to NULL*/
 }
