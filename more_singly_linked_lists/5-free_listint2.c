3299e3775263d14d9979ee1e8324c925c4f8e9f4
zack6yuan <zack6yuan@gmail.com>
2024-07-07 18:57:53 +0000

    commit

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,4 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
-
 /**
  * free_listint2 - frees a listint_t list, the function sets the head to NULL
  * @head: the head of the listint_t list
@@ -10,5 +6,17 @@
  */
 void free_listint2(listint_t **head)
 {
-    
+    listint_t *node;
+
+    if (head == NULL) /*parameter*/
+    {
+        return;
+    }
+    while (*head != NULL)
+    {
+    node = *head;
+    *head = (*head)->next;
+    free(node);
+    }
+    *head = NULL; /*head is set to NULL*/
 }
