fe4c040cb79d1807f022c7749a41b87a6b385c00
zack6yuan <zack6yuan@gmail.com>
2024-07-08 18:15:10 +0000

    question 3

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -2,11 +2,16 @@
 #include "main.h"
 /**
  * set_bit - sets the value of a bit to 1 at a given index
- * @n: 
- * @index:
+ * @n: number being changed
+ * @index: the index of the bit 
  * Return: 1 if successful, otherwise, -1 if error
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-
+if (index > 63) /*max val for (-) long int*/
+{
+return (-1); /*if error*/
+}
+*n = *n | (1 << index); /*sets bit at index */
+return (1); /*if successful*/
 }
