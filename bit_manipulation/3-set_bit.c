ab61b1e3bfea2371a94a5c27ea6c42bf5fc46754
zack6yuan <zack6yuan@gmail.com>
2024-07-08 18:16:09 +0000

    whitespace

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -3,7 +3,7 @@
 /**
  * set_bit - sets the value of a bit to 1 at a given index
  * @n: number being changed
- * @index: the index of the bit 
+ * @index: the index of the bit
  * Return: 1 if successful, otherwise, -1 if error
  */
 int set_bit(unsigned long int *n, unsigned int index)
