55dc19b148fd2991dd16fd7b46131e6c1eb806b4
zack6yuan <zack6yuan@gmail.com>
2024-07-08 22:16:13 +0000

    changes

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -7,5 +7,32 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int descriptor
+        int file_descriptor; /*stores descriptor*/
+        ssize_t readval; /*stores read result*/
+        ssize_t writeval; /*stores write result*/
+        char *buffer; /*pointer to char*/
+
+        if (filename == NULL || buffer == NULL)
+        {
+                return (0); 
+        }
+        file_descriptor = open(filename, O_RDONLY); /*attempt to open in read-only mode*/
+        if (file_descriptor == -1)
+        {
+                return (0);
+        }
+        buffer = malloc(sizeof(char) * (letters)); /*malloc for buffer to hold chars of  "letters"*/
+        if (buffer == NULL)
+        {
+                close(file_descriptor);
+                return (0);
+        }
+        readval = read(file_descriptor, buffer, letters);
+        if (readval == -1)
+        {
+                return (0);
+        }
+        writeval = write(STDOUT_FILENO, buffer, readval);
+        free(buffer);
+        return (writeval);
 }
