275a66db6048fad05311413b3dd211217d1cead9
zack6yuan <zack6yuan@gmail.com>
2024-07-08 20:07:46 +0000

    changes

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * flip_bits - returns the number of bits you would need to flip to advance numbers
- * @n:
- * @m:
+ * @n: the first value
+ * @m: the second value
  * Return: number of bits needed
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    
+int x;
+int count = 0;
+
+for (x = 0; x < 63; x++)
+{
+
+}
 }
---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: the name of the file that is being read
+ * @letters: the characters that are being read + printed
+ * Return: 0 if filename is NULL, if the file can't be opened, or if write fails
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int descriptor
+}
