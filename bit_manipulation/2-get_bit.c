bc3e0d40f73aba81ef59da94f30873b5751939ba
zack6yuan <zack6yuan@gmail.com>
2024-07-08 18:07:59 +0000

    question 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * get_bit - returns the value of a bit at a given index
  * @n: the given number
- * @index: 
+ * @index: index to find
  * Return: value of a the bit at the given index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 int value;
 
-if (index > 63)
+if (index > 63) /*max val for (-) long int*/
 {
-return (-1);
+return (-1); /*error*/
 }
-return ((n >> index) & 1);
+return ((n >> index) & 1); /*calculate value of bit at index*/
 }
