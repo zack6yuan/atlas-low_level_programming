f6da542f454b3fba53174ea27034d78170322501
zack6yuan <zack6yuan@gmail.com>
2024-07-08 18:35:19 +0000

    betty doc and new line

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -2,8 +2,8 @@
 #include "main.h"
 /**
  * clear_bit - sets the value of a bit to 0 at a given index
- * @n:
- * @index:
+ * @n: pointer
+ * @index: index value
  * Return: 1 if successful, otherwise, -1 if error
  */
 int clear_bit(unsigned long int *n, unsigned int index)
