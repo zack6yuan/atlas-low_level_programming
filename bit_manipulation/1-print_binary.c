0228d7455f32bc9c4daa3ed9bb07d6a502640514
zack6yuan <zack6yuan@gmail.com>
2024-07-09 01:26:29 +0000

    changes

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_binary - prints the binary representation of a number
@@ -7,5 +6,10 @@
  */
 void print_binary(unsigned long int n)
 {
-
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + '0');
 }
+
