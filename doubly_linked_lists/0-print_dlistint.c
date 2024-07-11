37c403d93f25a6f534aee28900aa3ba510627310
zack6yuan <zack6yuan@gmail.com>
2024-07-11 19:15:41 +0000

    structure

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: the head of the list
+ * Return: the number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+        
+}
---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints the number of elements of a dlistint_t list
+ * @h: the head of the list
+ * Return: the number of elements
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+        
+}
