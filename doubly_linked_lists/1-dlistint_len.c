41d8feb7f4389cc9e9868b2d48c8da5936d3d00f
zack6yuan <zack6yuan@gmail.com>
2024-07-12 20:13:59 +0000

    betty

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
- * dlistint_len - prints the number of elements of a dlistint_t list
- * @h: the head of the list
- * Return: the number of elements
+* dlistint_len - prints the number of elements of a dlistint_t list
+* @h: the head of the list
+* Return: the number of elements
 */
 size_t dlistint_len(const dlistint_t *h)
 {
 		    int nodes = 0; /*counter for nodes*/
 
-            while (h != NULL) /*parameter*/
+			while (h != NULL) /*parameter*/
 			{
 				nodes++; /*increment counter*/
 				h = h->next; /*next node*/
