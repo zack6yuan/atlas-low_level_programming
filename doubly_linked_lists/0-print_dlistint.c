83b88c773b3f84fe3eeacee505bc1c3cb87b7efe
zack6yuan <zack6yuan@gmail.com>
2024-07-12 19:02:03 +0000

    betty?

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,12 +8,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
         int nodes = 0; /*counter for nodes*/
-                
+
         while (h != NULL) /*parameter*/
         {
-                printf("%d\n", h->n); /*print the element's value*/
-                nodes++; /*increment the counter*/
-                h = h->next; /*move to the next node*/
+                    printf("%d\n", h->n); /*print the element's value*/
+                    nodes++; /*increment the counter*/
+                    h = h->next; /*move to the next node*/
         }
         return (nodes); /*return nodes*/
 }
