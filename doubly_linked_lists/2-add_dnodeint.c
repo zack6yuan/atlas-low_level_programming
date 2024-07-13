2d5e4158bca49fe86163f5b204d936b170507348
zack6yuan <zack6yuan@gmail.com>
2024-07-13 16:06:02 +0000

    betty

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * add_dnodeint - adds a new node at the beginning of a dlistint_t list
- * @head: the head of the list
- * @n: value to be assigned to new node
- * Return: address of the new element, or NULL if it failed
- */
+* add_dnodeint - adds a new node at the beginning of a dlistint_t list
+* @head: the head of the list
+* @n: value to be assigned to new node
+* Return: address of the new element, or NULL if it failed
+*/
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 		dlistint_t *new_node; /*ptr to new head*/
@@ -16,15 +16,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		{
 			return (NULL);
 		}
-                new_node->n = n; /*assign new node with value*/
-                new_node->next = *head; /*set new node to current head*/
-                new_node->prev = NULL; /*set prev to NULL*/
 
-                if (*head != NULL)
-                {
-                        (*head)->prev = new_node;
-                }
-                *head = new_node; /*assign to head ptr*/
+		new_node->n = n; /*assign new node with value*/
+		new_node->next = *head; /*set new node to current head*/
+		new_node->prev = NULL; /*set prev to NULL*/
 
-                return (*head); /*return address of new element*/
+	        if (*head != NULL)
+		{
+				(*head)->prev = new_node;
+		}
+		*head = new_node; /*assign to head ptr*/
+
+		return (*head); /*return address of new element*/
 }
