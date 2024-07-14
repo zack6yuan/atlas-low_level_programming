21d133072c3d8d18cc4f8e74ef89ea6c1470bcc6
zack6yuan <zack6yuan@gmail.com>
2024-07-14 06:40:56 +0000

    question 3 [incomplete]

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table to add or update key value to
+ * @key: the key, that can't be an empty string
+ * @value: the value associated with the key, that must be duplicated
+ * Return: 1 if succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+        
+}
