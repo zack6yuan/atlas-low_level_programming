bdd2a7c030730ac6f987709cb921bfaca2ebad85
zack6yuan <zack6yuan@gmail.com>
2024-07-14 06:46:09 +0000

    question 5 [incomplete]

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table, if it is NULL, don't print anything
+ * Return: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+        
+}
