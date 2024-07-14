5b62b92655cfe58976d0a626f82b2b0c615f0a61
zack6yuan <zack6yuan@gmail.com>
2024-07-14 06:44:10 +0000

    question 4 [incomplete]

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table to look into
+ * @key: the key being searched for
+ * Return: the value associated with the element
+ * or NULL if key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+}
