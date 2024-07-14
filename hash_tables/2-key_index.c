ec89b473da4b1718c7d70ec1cf4769baf9245732
zack6yuan <zack6yuan@gmail.com>
2024-07-14 06:37:33 +0000

    question 2 [incomplete]

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,5 +1,5 @@
 #include "hash_tables.h"
-/** 
+/**
  * key_index - gives the user the index of a key
  * @key: the key
  * @size: the size of the array of the hash table
@@ -8,5 +8,5 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-        
+
 }
