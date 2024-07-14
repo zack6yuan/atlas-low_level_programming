5a1be3ef6824f04c966b70022044476c5c974080
zack6yuan <zack6yuan@gmail.com>
2024-07-14 05:59:21 +0000

    betty doc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,6 +3,7 @@
  * hash_table_create - creates a hash table
  * @size: the size of the hash table
  * Return: pointer top the newly created hash table
+ * or...if something goes wrong, returns NULL
  */
 hash_table_t *hash_table_create(unsigned long int size);
 {
