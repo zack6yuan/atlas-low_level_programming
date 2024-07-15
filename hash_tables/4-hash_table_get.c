0511864b16a0c391a9319b1d8fa80f2e87f79b0e
zack6yuan <zack6yuan@gmail.com>
2024-07-15 17:03:10 +0000

    question 4 [compelete]

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,12 +1,31 @@
 #include "hash_tables.h"
+#include <string.h>
 /**
- * hash_table_get - retrieves a value associated with a key
- * @ht: the hash table to look into
- * @key: the key being searched for
- * Return: the value associated with the element
- * or NULL if key couldn't be found
- */
+* hash_table_get - retrieves a value associated with a key
+* @ht: the hash table to look into
+* @key: the key being searched for
+* Return: the value associated with the element
+* or NULL if key couldn't be found
+*/
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+		unsigned long int idx;
+		hash_node_t *node;
 
+                if (ht == NULL || key == NULL)
+                {
+                        return (NULL);
+                }
+                idx = key_index((const unsigned char *) key, ht->size);
+                node = ht->array[index];
+
+                while (node) /*while node != NULL*/
+                {
+                        if (strcmp(node->key, key) == 0)
+                        {
+                                return (node->value); /*return value*/
+                                node = node->next; /*next node*/
+                        }
+                }
+                return (NULL); /*if key is not found*/
 }
