7ae67ef4eb776d8fc48cd1a579ac0ec3f5bfd027
zack6yuan <zack6yuan@gmail.com>
2024-07-15 16:49:54 +0000

    question 2 [complete]

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -8,5 +8,9 @@
 */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+                unsigned long int idx;
 
+                idx = hash_djb2(key);
+
+                return (idx % key);
 }
