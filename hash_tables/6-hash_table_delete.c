a8781033329384f87fd0cc81342484d233c74e01
zack6yuan <zack6yuan@gmail.com>
2024-07-16 01:11:24 +0000

    question 6 [complete]

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 /**
  * hash_table_delete - deletes a hash table
  * @ht: the hash table
@@ -6,5 +7,26 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-        
+                hash_node_t *temp_node
+                hash_node_t *next_node
+                unsigned int x;
+
+                if (ht == NULL)
+                {
+                        return; /*return nothing*/
+                }
+                for (x = 0; x < ht->size; x++)
+                {
+                        temp_node = ht->array[x];
+                        while (temp_node != NULL) /*parameter*/
+                        {
+                                next_node = temp_node->next;
+                                free(temp_node->key); /*free the key*/
+                                free(temp_node->value); /*free the value*/
+                                free(temp_node); /*free temp node*/
+                                temp_node = next_node; /*temp = next*/
+                        }
+                }
+                free(ht->array); /*free the array*/
+                free(ht); /*free the hash table*/
 }
