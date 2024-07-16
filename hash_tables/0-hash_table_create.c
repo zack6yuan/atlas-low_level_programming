d1c56b4ef4c953ccaea83e3f9dce3e14257f0460
zack6yuan <zack6yuan@gmail.com>
2024-07-16 03:28:22 +0000

    question 0 [complete]

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -16,6 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
                 {
                         return (NULL);
                 }
+                new_table->size = size; 
                 new_table->array = malloc(sizeof(hash_node_t *) * size); /*malloc for array*/
                 if (!new_table->array) /*if malloc fails*/
                 {
