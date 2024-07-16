1cf24d838f28a159b024cfb6d2241f4134db12a9
zack6yuan <zack6yuan@gmail.com>
2024-07-16 03:38:01 +0000

    betty

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,24 +8,24 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-		hash_table_t *new_table; /*new table pointer*/
-		unsigned long int x; /*used to iterate through table*/
+	hash_table_t *new_table; /*new table pointer*/
+	unsigned long int x; /*used to iterate through table*/
 
-		new_table = malloc(sizeof(hash_table_t)); /*malloc for new_table*/
-		if (!new_table) /*if NULL, return NULL*/
-		{
-				return (NULL);
-		}
-		new_table->size = size;
-		new_table->array = malloc(sizeof(hash_node_t *) * size); /*malloc for array*/
-		if (!new_table->array) /*if malloc fails*/
-		{
-				free(new_table); /*free memory*/
-				return (NULL);
-		}
-		for (x = 0; x < size; x++) /*iterate through each slot*/
-		{
-				new_table->array[x] = NULL;
-		}
-		return (new_table); /*return new_table*/
+	new_table = malloc(sizeof(hash_table_t)); /*malloc for new_table*/
+	if (!new_table) /*if NULL, return NULL*/
+	{
+		return (NULL);
+	}
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size); /*malloc for array*/
+	if (!new_table->array) /*if malloc fails*/
+	{
+		free(new_table); /*free memory*/
+		return (NULL);
+	}
+	for (x = 0; x < size; x++) /*iterate through each slot*/
+	{
+                new_table->array[x] = NULL;
+	}
+	return (new_table); /*return new_table*/
 }
