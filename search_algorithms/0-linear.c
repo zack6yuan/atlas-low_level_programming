288d57be0fb819d227dca96522db631cb20a74a7
zack6yuan <zack6yuan@gmail.com>
2024-08-07 21:52:51 +0000

    commit

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * linear_search - searches for a value in an array...
+ * of integers using the Linear Search Algorithm.
+ * @array: a pointer to the first element of the array to seach in.
+ * @size: the number of elements in "array."
+ * @value: the value to search for.
+ * Return: -1 if value is not present in array, or if array is NULL.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+
+}
---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * linear_search - searches for a value in a sorted array...
+ * of integers using the Binary Search Algorithm.
+ * @array: a pointer to the first element of the array to seach in.
+ * @size: the number of elements in "array."
+ * @value: the value to search for.
+ * Return: the index where value is located.
+ * -1 if value is not present in array, or is array is NULL.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+
+}
