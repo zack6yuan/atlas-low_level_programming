05b54c3f61dc55a5101a4eaabea5b2b18985e254
zack6yuan <zack6yuan@gmail.com>
2024-08-10 19:11:44 +0000

    added to main

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -8,5 +8,6 @@
 /*Function Prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t start, size_t end);
 
 #endif /*SEARCH_ALGOS_H*/
