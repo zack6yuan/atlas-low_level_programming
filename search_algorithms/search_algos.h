56fc0892c3bf4755023d1dc95f766385bd348817
zack6yuan <zack6yuan@gmail.com>
2024-08-10 18:37:51 +0000

    semicolons

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,7 +6,7 @@
 #include <stddef.h>
 
 /*Function Prototypes*/
-int linear_search(int *array, size_t size, int value)
-int binary_search(int *array, size_t size, int value)
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif /*SEARCH_ALGOS_H*/
