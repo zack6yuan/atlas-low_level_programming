2d90c33b243a3e74d12af65e429802fd51f62b39
zack6yuan <zack6yuan@gmail.com>
2024-08-10 19:19:22 +0000

    betty

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -12,33 +12,33 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-        size_t left_index = 0;
-        size_t mid_index;
-        size_t right_index = size - 1;
+size_t left_index = 0;
+size_t mid_index;
+size_t right_index = size - 1;
 
-        if (!array) /*check if array is NULL*/
-        {
-                return (-1); /*error*/
-        }
-        while (left_index <= right_index) /*binary search loop*/
-        {
-                print_array(array, left_index, right_index); /*calls function below*/
-                mid_index = (left_index + right_index) / 2; /*calculate mid index*/
+if (!array) /*check if array is NULL*/
+{
+return (-1); /*error*/
+}
+while (left_index <= right_index) /*binary search loop*/
+{
+print_array(array, left_index, right_index); /*calls function below*/
+mid_index = (left_index + right_index) / 2; /*calculate mid index*/
 
-                if (array[mid_index] < value) /*if left*/
-                {
-                        left_index = mid_index + 1; /*move to mid*/
-                }
-                else if (array[mid_index] > value) /*if right*/
-                {
-                        right_index = mid_index - 1; /*move to mid*/
-                }
-                else
-                {
-                        return (mid_index); /*return index*/
-                }
-        }
-        return (-1); /*not found*/
+if (array[mid_index] < value) /*if left*/
+{
+left_index = mid_index + 1; /*move to mid*/
+}
+else if (array[mid_index] > value) /*if right*/
+{
+right_index = mid_index - 1; /*move to mid*/
+}
+else
+{
+return (mid_index); /*return index*/
+}
+}
+return (-1); /*not found*/
 }
 
 /**
@@ -50,16 +50,16 @@ int binary_search(int *array, size_t size, int value)
  */
 void print_array(int *array, size_t start, size_t end)
 {
-        size_t index;
+size_t index;
 
-        printf("Searching in array: ");
-        for (index = start; index <= end; index++) /*loop from start to end*/
-        {
-                if (index != start) /*check if first element*/
-                {
-                        printf(", "); /*seperate elements*/
-                }
-                printf("%d", array[index]); /*prints current element*/
-        }
-        printf("\n"); /*newline*/
+printf("Searching in array: ");
+for (index = start; index <= end; index++) /*loop from start to end*/
+{
+if (index != start) /*check if first element*/
+{
+printf(", "); /*seperate elements*/
+}
+printf("%d", array[index]); /*prints current element*/
+}
+printf("\n"); /*newline*/
 }
