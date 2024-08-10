70420bce053bde649f12cbde9d510f55df253e9e
zack6yuan <zack6yuan@gmail.com>
2024-08-10 19:14:23 +0000

    forgot semicolon

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -35,7 +35,7 @@ int binary_search(int *array, size_t size, int value)
                 }
                 else
                 {
-                        return (mid_index) /*return index*/
+                        return (mid_index); /*return index*/
                 }
         }
         return (-1); /*not found*/
