632aa305618b2a4cd3b603be2ba4d35b2fc4340e
zack6yuan <zack6yuan@gmail.com>
2024-08-10 18:37:06 +0000

    semicolon removed

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -18,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
         {
                 return (-1); /*error*/
         }
-        for (i = 0; i < size; i++); /*loop through array*/
+        for (i = 0; i < size; i++) /*loop through array*/
         {
                 printf("Value checked array[%lu] = [%d]\n", i, array[i]);
                 if (array[i] == value)
