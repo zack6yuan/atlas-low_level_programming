2ba24c9ec549d0c4e40b416c8e940e8237a2e56d
zack6yuan <zack6yuan@gmail.com>
2024-08-10 18:28:28 +0000

    0-linear.c completed.

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -7,9 +7,23 @@
  * @array: a pointer to the first element of the array to seach in.
  * @size: the number of elements in "array."
  * @value: the value to search for.
- * Return: -1 if value is not present in array, or if array is NULL.
+ * Return: the first index where "value" is located
+ * -1 if value is not present in array, or if array is NULL.
  */
 int linear_search(int *array, size_t size, int value)
 {
+        size_t i; /*counter*/
 
+        if (!array)
+        {
+                return (-1); /*error*/
+        }
+        for (i = 0; i < size; i++); /*loop through array*/
+        {
+                printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+                if (array[i] == value)
+                {
+                        return (i); /*return index*/
+                }
+        }
 }
